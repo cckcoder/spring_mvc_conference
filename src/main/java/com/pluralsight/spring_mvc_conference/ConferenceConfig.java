e588b86c06268c81cca92cfd613ce0cc9b1bcab3
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-20 16:14:41 +0700

    update: complete views in spring mvc

--- src/main/java/com/pluralsight/spring_mvc_conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/spring_mvc_conference/ConferenceConfig.java
@@ -3,10 +3,18 @@ package com.pluralsight.spring_mvc_conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
